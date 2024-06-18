33365c5647782b709fede8a53d67495ea7cb151a
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-06-18 22:09:25 +0100

    Remove context selector from generators/config
    The context selector was removed from the 'generators/config.ts' file. This choice was made since the context selector is no longer necessary in the current configuration schematics.

--- generators/config.ts
+++ generators/config.ts
@@ -2,13 +2,11 @@ import { PlopTypes } from '@turbo/gen';
 
 import { addContextGenerator } from './generators/context';
 import { addSourceRootHelper } from './helpers/source-root';
-import { addContextSelector } from './prompts/context-selector';
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
     plop.setWelcomeMessage('Welcome to Enterprise Nest! 🚀');
 
     addSourceRootHelper(plop);
-    addContextSelector(plop);
 
     addContextGenerator(plop);
 }
