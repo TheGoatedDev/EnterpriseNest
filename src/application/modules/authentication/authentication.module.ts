7dc313fec0214842d39290f4159a314d7f25e341
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-06-26 21:59:39 +0100

    Add RefreshTokenStrategy to AuthenticationModule
    A new strategy called RefreshTokenStrategy is added to the authentication module. This is accompanied by the creation of the corresponding refreshToken Guard and Strategy files. The new strategy is now authorized and handled within the authentication module.

--- src/application/modules/authentication/authentication.module.ts
+++ src/application/modules/authentication/authentication.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { OnValidateCredentialsWhenPasswordIncorrectAddToCounterHandler } from '@/application/modules/authentication/event-handlers/on-validate-credentials-when-password-incorrect-add-to-counter.handler';
 import { AccessTokenStrategy } from '@/application/modules/authentication/strategies/access-token/access-token.strategy';
 import { LocalStrategy } from '@/application/modules/authentication/strategies/local/local.strategy';
+import { RefreshTokenStrategy } from '@/application/modules/authentication/strategies/refresh-token/refresh-token.strategy';
 import { V1AuthenticationModule } from '@/application/modules/authentication/v1/v1-authentication.module';
 
 const EventHandlers: Type[] = [
@@ -13,6 +14,11 @@ const EventHandlers: Type[] = [
 @Module({
     imports: [PassportModule, V1AuthenticationModule],
 
-    providers: [...EventHandlers, LocalStrategy, AccessTokenStrategy],
+    providers: [
+        ...EventHandlers,
+        LocalStrategy,
+        AccessTokenStrategy,
+        RefreshTokenStrategy,
+    ],
 })
 export class AuthenticationModule {}
---
+++ src/application/modules/authentication/strategies/refresh-token/refresh-token.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
+
+import { AuthenticationConfigService } from '@/application/system/config/configs/authentication-config.service';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+    Strategy,
+    'refreshToken',
+) {
+    constructor(
+        private readonly queryBus: QueryBus,
+        private readonly authenticationConfig: AuthenticationConfigService,
+    ) {
+        const options: StrategyOptions = {
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: authenticationConfig.jwtSecret,
+        };
+
+        super(options);
+    }
+
+    validate(payload: unknown): Promise<unknown> {
+        return Promise.resolve(payload);
+    }
+}
