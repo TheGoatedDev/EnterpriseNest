e628f948975b559bc732273db18048bac798f937
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-06-28 16:25:01 +0100

    Add email verification confirmation command
    This commit adds a new command for email verification confirmation. It includes the command, request and response DTOs, the related command handler, and a related HTTP controller. It aims to securely verify the user's email address using an embedded token, improving account security.

---
+++ src/application/verification/v1/commands/confirm-verification/confirm-verification.handler.ts
@@ -0,0 +1,97 @@
+import {
+    BadRequestException,
+    ForbiddenException,
+    Logger,
+} from '@nestjs/common';
+import {
+    CommandBus,
+    CommandHandler,
+    EventBus,
+    ICommandHandler,
+    QueryBus,
+} from '@nestjs/cqrs';
+import { JwtService } from '@nestjs/jwt';
+
+import { V1UpdateUserCommandHandler } from '@/application/user/v1/commands/update-user/update-user.handler';
+import { V1FindUserByIDQueryHandler } from '@/application/user/v1/queries/find-user-by-id/find-user-by-id.handler';
+import { User } from '@/domain/user/user.entity';
+import { OnVerificationConfirmedEvent } from '@/domain/verification/events/on-verification-confirmed.event';
+import { VerifyEmailTokenPayload } from '@/domain/verification/verify-email-token-payload.type';
+
+import { V1ConfirmVerificationCommand } from './confirm-verification.command';
+
+interface V1ConfirmVerificationCommandHandlerResponse {
+    user: User;
+}
+
+@CommandHandler(V1ConfirmVerificationCommand)
+export class V1ConfirmVerificationCommandHandler
+    implements
+        ICommandHandler<
+            V1ConfirmVerificationCommand,
+            V1ConfirmVerificationCommandHandlerResponse
+        >
+{
+    private readonly logger = new Logger(
+        V1ConfirmVerificationCommandHandler.name,
+    );
+
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly eventBus: EventBus,
+        private readonly commandBus: CommandBus,
+        private readonly queryBus: QueryBus,
+    ) {}
+
+    static runHandler(
+        bus: CommandBus,
+        command: V1ConfirmVerificationCommand,
+    ): Promise<V1ConfirmVerificationCommandHandlerResponse> {
+        return bus.execute<
+            V1ConfirmVerificationCommand,
+            V1ConfirmVerificationCommandHandlerResponse
+        >(new V1ConfirmVerificationCommand(command.verificationToken));
+    }
+
+    async execute(
+        command: V1ConfirmVerificationCommand,
+    ): Promise<V1ConfirmVerificationCommandHandlerResponse> {
+        this.logger.log(
+            `Confirming verification for ${command.verificationToken}`,
+        );
+
+        const payload = await this.jwtService
+            .verifyAsync<VerifyEmailTokenPayload>(command.verificationToken)
+            .catch(() => {
+                this.logger.error('Invalid Token');
+                throw new BadRequestException('Invalid Token');
+            });
+
+        if (payload.type !== 'verify-email') {
+            throw new ForbiddenException('Invalid Token Type');
+        }
+
+        const user = await V1FindUserByIDQueryHandler.runHandler(
+            this.queryBus,
+            {
+                id: payload.data.sub,
+            },
+        );
+
+        if (!user) {
+            throw new ForbiddenException('User not found or Token is invalid');
+        }
+
+        user.verifiedAt = new Date();
+
+        await V1UpdateUserCommandHandler.runHandler(this.commandBus, {
+            user,
+        });
+
+        this.eventBus.publish(new OnVerificationConfirmedEvent(user));
+
+        return Promise.resolve({
+            user,
+        });
+    }
+}
