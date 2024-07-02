f30c64b1a7653e24b5b8c52a752fc86e65a16d8c
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-07-02 12:14:54 +0100

    Remove refreshToken.request.dto file
    The refreshToken.request.dto file in the v1 commands has been removed. This file no longer was necessary as the application code has been refactored, eliminating the need for this specific data transfer object. The user email and password are now handled differently.

--- src/application/authentication/v1/commands/refresh-token/dto/refresh-token.request.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IntersectionType } from '@nestjs/swagger';
-
-import { UserEmailDto, UserPasswordDto } from '@/domain/user/user.dto';
-
-export class V1RefreshTokenRequestDto extends IntersectionType(
-    UserEmailDto,
-    UserPasswordDto,
-) {}
