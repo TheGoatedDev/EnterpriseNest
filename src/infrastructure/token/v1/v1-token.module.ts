545424bc1b9d69e744280a34589a0aa1953109c4
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-07-04 11:50:41 +0100

    Add verification token generation handler
    A new handler for generating verification tokens has been added to the command handlers in the v1-token module. This enhancement ensures the module supports all necessary token generation functionalities, including access, refresh, and verification tokens.

--- src/infrastructure/token/v1/v1-token.module.ts
+++ src/infrastructure/token/v1/v1-token.module.ts
@@ -2,6 +2,7 @@ import { Module, Type } from '@nestjs/common';
 
 import { V1GenerateAccessTokenCommandHandler } from '@/infrastructure/token/v1/commands/generate-access-token/generate-access-token.handler';
 import { V1GenerateRefreshTokenCommandHandler } from '@/infrastructure/token/v1/commands/generate-refresh-token/generate-refresh-token.handler';
+import { V1GenerateVerificationTokenCommandHandler } from '@/infrastructure/token/v1/commands/generate-verification-token/generate-verification-token.handler';
 
 const QueryHandlers: Type[] = [];
 const QueryControllers: Type[] = [];
@@ -9,6 +10,7 @@ const QueryControllers: Type[] = [];
 const CommandHandlers: Type[] = [
     V1GenerateAccessTokenCommandHandler,
     V1GenerateRefreshTokenCommandHandler,
+    V1GenerateVerificationTokenCommandHandler,
 ];
 const CommandControllers: Type[] = [];
 
