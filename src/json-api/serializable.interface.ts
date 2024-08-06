4902f422aa3265c66a2481e89381268dcae0fcdc
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-08-06 16:33:31 +0100

    Gone

--- src/json-api/serializable.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface SerializableInterface<T> {
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    serialize(): Record<string, any>;
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    deserialize(data: Record<string, any>): T;
-}
